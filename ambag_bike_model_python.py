a0af04f32176444e7580bd808c96a190b37ab692
Blake Rosenthal <blake.rosenthal@rsginc.com>
2020-03-31 15:23:26 -0700

    update run script

--- ambag_bike_model_python.py
+++ ambag_bike_model_python.py
@@ -3,33 +3,41 @@ import time
 
 from activitysim.core import pipeline
 from activitysim.core import inject
+from activitysim.core.config import setting
 
 from bike_demand import incremental_demand
 from bike_demand import benefits
 from bike_demand import assign_demand
 
-usage = "ambag_bike_model_python.py {--type [incremental_demand/benefits/assign_demand]} --base [base_database] --build [build_database] [--base_disk]}"
-if __name__ == '__main__':
+
+def run():
 
     t1 = time.time()
 
+    models = [
+        'initial_demand',
+        'incremental_demand',
+        'benefits',
+        'assign_demand'
+    ]
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('--type',dest='type',action='store')
+    parser.add_argument('--type', dest='type', action='store', choices=models)
     args = parser.parse_args()
 
     inject.add_injectable('configs_dir', 'ambag_example/configs')
     inject.add_injectable('data_dir', 'ambag_example/data')
     inject.add_injectable('output_dir', 'ambag_example/output')
 
-    model = args.type
-    if model == "incremental_demand":
-        pipeline.run(models=['incremental_demand'])
-        pipeline.close_pipeline()
-    elif model == "benefits":
-        benefits.benefits_main()
-    elif model == "assign_demand":
-        assign_demand.assign_demand_main()
+    if args.type:
+        pipeline.run(models=[args.type])
     else:
-        print(usage)
+        pipeline.run(models=setting('models'))
+
+    pipeline.close_pipeline()
 
     print('runtime: ', time.time() - t1)
+
+
+if __name__ == '__main__':
+    run()
