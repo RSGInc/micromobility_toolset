94cb19f7e134abe9bbbe34bee1f324f5208cddce
Blake Rosenthal <blake.rosenthal@rsginc.com>
2021-04-14 15:25:18 -0700

    bump version to 0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="micromobility_toolset",
-    version="0.3",
+    version="0.3.1",
     description="Micromobility Travel Modeling Toolkit",
     author="contributing authors",
     author_email="blake.rosenthal@rsginc.com",
